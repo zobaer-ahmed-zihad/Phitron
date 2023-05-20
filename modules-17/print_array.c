5d1a20c50df4a442e2233a185242fb3240b285f8
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2023-05-20 10:22:30 +0600

    17-5 Array Printing using Recursion

---
+++ modules-17/print_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void fun(int ar[],int n, int i){
+    //base case
+    if(i ==n ) return;
+    printf("%d ", ar[i]);
+    fun(ar, n, i+1);
+}
+
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int ar[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &ar[i]);
+    }
+    fun(ar, n, 0);
+    
+    return 0;
+}
