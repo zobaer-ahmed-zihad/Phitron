2bed30bfccc3b5867cb21740a1dabacbe7cf6da1
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2023-05-21 21:14:13 +0600

    First C++ program

--- Introduction_to_C++_for_DSA/module-01/Hello.cpp
+++ Introduction_to_C++_for_DSA/module-01/Hello.cpp
@@ -1,4 +1,11 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
+
 int main(){
-    printf("Hello world !");
+    // std::cout<<"Hello world!";
+    // std::cout<<"Hello "<< "world!";
+    cout<<"Hello" << endl << "world!"<<endl;
+    int a =  47;
+    cout<<a; // printf a variable value
+    return 0;
 }
