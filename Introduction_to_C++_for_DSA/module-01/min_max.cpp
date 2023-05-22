f619e4840fd06f5fad9f7b2597cd1ebae18fae67
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2023-05-22 22:15:26 +0600

    max min using manual and build in function

--- Introduction_to_C++_for_DSA/module-01/min_max.cpp
+++ Introduction_to_C++_for_DSA/module-01/min_max.cpp
@@ -2,12 +2,26 @@
 #include<algorithm>
 using namespace std;
 
+// int my_min(int a, int b){
+//     if(a < b) return a;
+//     else return b;
+// }
+// int my_max(int a, int b){
+//     if(a > b) return a;
+//     else return b;
+// }
+
 int main()
 {
     int a, b;
     cin>> a >>b;
-    int mn = min(a,b);
-    int mx = max(a,b);
+    // int minimum = my_min(a,b);
+    // int maximum = my_max(a, b);
+
+    // cout<<minimum<< " "<< maximum;
+
+    int mn = min(a,b); // use build in function
+    int mx = max(a,b); // use build in function
 
     cout<<"Minimum "<<mn<<endl<<"Maximum "<<mx;
 }
