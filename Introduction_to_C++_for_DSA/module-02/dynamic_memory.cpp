b3defe7c2c6c1fb089331f09676f096593b9e855
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2023-05-24 15:11:41 +0600

    2-2 Create Data in Dynamic Memory

---
+++ Introduction_to_C++_for_DSA/module-02/dynamic_memory.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int *a = new int; // dynamic memory allocation
+    *a = 10;
+    cout<<*a<<endl;
+
+    float *b = new float;
+    *b = 7.23;
+    cout<<*b<<endl;
+    return 0;
+}
