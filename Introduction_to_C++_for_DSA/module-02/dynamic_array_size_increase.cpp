828c2f10c945c9cd46c560576b628f9463b4e8d7
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2023-05-25 23:14:09 +0600

    Increase Size using Dynamic Array

---
+++ Introduction_to_C++_for_DSA/module-02/dynamic_array_size_increase.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int *a = new int [5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>a[i];
+    }
+    int *b = new int [7];
+    for (int i = 0; i < 5; i++)
+    {
+        b[i] = a[i];
+    }
+    b[5] = 77;
+    b[6] = 80;
+
+    for (int i = 0; i < 7; i++)
+    {
+        cout<<b[i]<<" ";
+    }
+    cout<<endl;
+    // delete a array
+    delete[] a;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    
+    
+
+
+    return 0;
+}
