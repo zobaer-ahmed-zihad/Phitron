a0930ee872bf8dd23b04958765087eca8f8f8f14
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2023-10-26 10:51:20 +0600

    Setprecision in c++

--- Introduction_to_C++_for_DSA/module-01/float_output.cpp
+++ Introduction_to_C++_for_DSA/module-01/float_output.cpp
@@ -4,8 +4,10 @@
 using namespace std;
 int main(){
     float a;
+    double b = 9.24568;
     cin>>a;
-    cout<< fixed << setprecision(2) << a;
+    cout<< fixed << setprecision(2) << a<<endl;
+    cout<< fixed << setprecision(3)<< b<<endl;
 
     return 0;
 }
