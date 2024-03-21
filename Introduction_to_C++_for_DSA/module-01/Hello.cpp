7b8fc4f14d3cea27d41178f668fbc3cdc28a882b
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2024-03-21 13:56:17 +0600

    Revise those things

--- Introduction_to_C++_for_DSA/module-01/Hello.cpp
+++ Introduction_to_C++_for_DSA/module-01/Hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
     // std::cout<<"Hello world!";
     // std::cout<<"Hello "<< "world!";
+    cout<<"This is my First c++ program."<<endl;
     cout<<"Hello" << endl << "world!"<<endl;
     int a =  47;
     cout<<a; // printf a variable value
